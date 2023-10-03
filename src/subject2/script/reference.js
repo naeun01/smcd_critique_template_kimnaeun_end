63ab390f4cd5b19ab145d8717c969d7db8d95958
naeun01 <101083406+naeun01@users.noreply.github.com>
2023-10-03 11:34:08 +0900

    다시만든것

--- src/subject2/script/reference.js
+++ src/subject2/script/reference.js
@@ -1,30 +1,9 @@
 const reference = [
   {
-    author: "lena polishko",
-    title: "",
+    author: "최소혜",
+    title: "201921418",
     year: "",
     sourceTitle: "",
-    link: "https://unsplash.com/@ilon_a",
+    link: "",
   },
-  {
-    author: "ash thorp",
-    title: "M O D U L A R - YouTube",
-    year: "2021",
-    sourceTitle: "",
-    link: "https://vimeo.com/486511696",
-  },
-  {
-    author: "ash thorp",
-    title: "FITC Tokyo 2015 Titles",
-    year: "2015",
-    sourceTitle: "",
-    link: "https://vimeo.com/118919656",
-  },
-  {
-    author: "GitHub, Inc.",
-    title: "What is GitHub?",
-    year: "2022",
-    sourceTitle: "",
-    link: "https://www.youtube.com/watch?v=pBy1zgt0XPc",
-  },
-];
+]
